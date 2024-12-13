b81c7f4368de4d4b85fa9d91f7fe6556c77a78bf
Gaolingx <947770192@qq.com>
2024-12-13 16:38:51 +0800

    Update SRUniversalUtils.hlsl
    format document
    
    Update SRUniversalUtils.hlsl

--- Shaders/SRUniversal-main/ShaderLibrary/SRUniversalUtils.hlsl
+++ Shaders/SRUniversal-main/ShaderLibrary/SRUniversalUtils.hlsl
@@ -5,7 +5,6 @@
 #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
 #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/DeclareDepthTexture.hlsl"
 
-
 // utils ---------------------------------------------------------------------------------------------------------- //
 // ---------------------------------------------------------------------------------------------------------------- //
 float4 CombineAndTransformDualFaceUV(float2 uv1, float2 uv2, float4 mapST)
@@ -15,21 +14,21 @@ float4 CombineAndTransformDualFaceUV(float2 uv1, float2 uv2, float4 mapST)
 
 void SetupDualFaceRendering(inout float3 normalWS, inout float4 uv, FRONT_FACE_TYPE isFrontFace)
 {
-    #if defined(_CUSTOMHEADBONEMODEVARENUM_GAME)
-        if (IS_FRONT_VFACE(isFrontFace, 1, 0))
-            return;
+#if defined(_CUSTOMHEADBONEMODEVARENUM_GAME)
+    if (IS_FRONT_VFACE(isFrontFace, 1, 0))
+        return;
 
-        // 游戏内的部分模型用了双面渲染
-        // 渲染背面的时候需要调整一些值，这样就不需要修改之后的计算了
+    // 游戏内的部分模型用了双面渲染
+    // 渲染背面的时候需要调整一些值，这样就不需要修改之后的计算了
 
-        // 反向法线
-        normalWS *= -1;
+    // 反向法线
+    normalWS *= -1;
 
-        // 交换 uv1 和 uv2
-        #if defined(_BACKFACEUV2_ON)
-            uv.xyzw = uv.zwxy;
-        #endif
-    #endif
+// 交换 uv1 和 uv2
+#if defined(_BACKFACEUV2_ON)
+    uv.xyzw = uv.zwxy;
+#endif
+#endif
 }
 
 float GetLinearEyeDepthAnyProjection(float depth)
@@ -88,16 +87,15 @@ float3 LerpRampColor(float3 coolRamp, float3 warmRamp, float dayTime, float shad
 
 void DoClipTestToTargetAlphaValue(float alpha, float alphaTestThreshold)
 {
-    #if _ALPHATEST_ON
-        clip(alpha - alphaTestThreshold);
-    #endif
+#if _ALPHATEST_ON
+    clip(alpha - alphaTestThreshold);
+#endif
 }
 
 float3 MixBloomColor(float3 colorTarget, float3 bloomColor, float bloomIntensity)
 {
     return colorTarget * (1 + max(0, bloomIntensity) * bloomColor);
 }
-
 
 // HeadDirections ------------------------------------------------------------------------------------------------- //
 // ---------------------------------------------------------------------------------------------------------------- //
@@ -115,21 +113,21 @@ HeadDirections GetWorldSpaceCharHeadDirectionsImpl(
 {
     HeadDirections dirWS;
 
-    #if defined(_CUSTOMHEADBONEMODEVARENUM_GAME)
-        // 游戏模型的头骨骼是旋转过的
-        dirWS.forward = normalize(UNITY_MATRIX_M._m01_m11_m21); // +Y 是 Forward
-        dirWS.right = normalize(-UNITY_MATRIX_M._m02_m12_m22);  // -Z 是 Right
-        dirWS.up = normalize(-UNITY_MATRIX_M._m00_m10_m20);     // -X 是 Up
-    #elif defined(_CUSTOMHEADBONEMODEVARENUM_MMD)
-        // MMD 模型只有一个根骨骼上的 Renderer，头骨骼信息需要额外获取
-        dirWS.forward = mmdHeadBoneForward.xyz;
-        dirWS.right = mmdHeadBoneRight.xyz;
-        dirWS.up = mmdHeadBoneUp.xyz;
-    #else
-        dirWS.forward = normalize(UNITY_MATRIX_M._m02_m12_m22); // 其他情况下是 +Z
-        dirWS.right = normalize(UNITY_MATRIX_M._m00_m10_m20);   // 其他情况下是 +X
-        dirWS.up = normalize(UNITY_MATRIX_M._m01_m11_m21);      // 其他情况下是 +Y
-    #endif
+#if defined(_CUSTOMHEADBONEMODEVARENUM_GAME)
+    // 游戏模型的头骨骼是旋转过的
+    dirWS.forward = normalize(UNITY_MATRIX_M._m01_m11_m21); // +Y 是 Forward
+    dirWS.right = normalize(-UNITY_MATRIX_M._m02_m12_m22);  // -Z 是 Right
+    dirWS.up = normalize(-UNITY_MATRIX_M._m00_m10_m20);     // -X 是 Up
+#elif defined(_CUSTOMHEADBONEMODEVARENUM_MMD)
+    // MMD 模型只有一个根骨骼上的 Renderer，头骨骼信息需要额外获取
+    dirWS.forward = mmdHeadBoneForward.xyz;
+    dirWS.right = mmdHeadBoneRight.xyz;
+    dirWS.up = mmdHeadBoneUp.xyz;
+#else
+    dirWS.forward = normalize(UNITY_MATRIX_M._m02_m12_m22); // 其他情况下是 +Z
+    dirWS.right = normalize(UNITY_MATRIX_M._m00_m10_m20);   // 其他情况下是 +X
+    dirWS.up = normalize(UNITY_MATRIX_M._m01_m11_m21);      // 其他情况下是 +Y
+#endif
 
     return dirWS;
 }
@@ -137,24 +135,22 @@ HeadDirections GetWorldSpaceCharHeadDirectionsImpl(
 #define WORLD_SPACE_CHAR_HEAD_DIRECTIONS() \
     GetWorldSpaceCharHeadDirectionsImpl(_MMDHeadBoneForward, _MMDHeadBoneUp, _MMDHeadBoneRight)
 
-
 // DitherAlpha ---------------------------------------------------------------------------------------------------- //
 // ---------------------------------------------------------------------------------------------------------------- //
 void DoDitherAlphaEffect(float4 svPosition, float ditherAlpha)
 {
     static const float4 thresholds[4] =
-    {
-        float4(01.0 / 17.0, 09.0 / 17.0, 03.0 / 17.0, 11.0 / 17.0),
-        float4(13.0 / 17.0, 05.0 / 17.0, 15.0 / 17.0, 07.0 / 17.0),
-        float4(04.0 / 17.0, 12.0 / 17.0, 02.0 / 17.0, 10.0 / 17.0),
-        float4(16.0 / 17.0, 08.0 / 17.0, 14.0 / 17.0, 06.0 / 17.0)
-    };
+        {
+            float4(01.0 / 17.0, 09.0 / 17.0, 03.0 / 17.0, 11.0 / 17.0),
+            float4(13.0 / 17.0, 05.0 / 17.0, 15.0 / 17.0, 07.0 / 17.0),
+            float4(04.0 / 17.0, 12.0 / 17.0, 02.0 / 17.0, 10.0 / 17.0),
+            float4(16.0 / 17.0, 08.0 / 17.0, 14.0 / 17.0, 06.0 / 17.0),
+        };
 
     uint xIndex = fmod(svPosition.x - 0.5, 4);
     uint yIndex = fmod(svPosition.y - 0.5, 4);
     clip(ditherAlpha - thresholds[yIndex][xIndex]);
 }
-
 
 // CharacterMainLight --------------------------------------------------------------------------------------------- //
 // ---------------------------------------------------------------------------------------------------------------- //
@@ -162,97 +158,95 @@ Light GetCharacterMainLightStruct(float4 shadowCoord, float3 positionWS)
 {
     Light light = GetMainLight();
 
-    #if defined(MAIN_LIGHT_CALCULATE_SHADOWS)
-        ShadowSamplingData shadowSamplingData = GetMainLightShadowSamplingData();
-        half4 shadowParams = GetMainLightShadowParams();
+#if defined(MAIN_LIGHT_CALCULATE_SHADOWS)
+    ShadowSamplingData shadowSamplingData = GetMainLightShadowSamplingData();
+    half4 shadowParams = GetMainLightShadowParams();
 
-        // 我自己试下来，在角色身上 LowQuality 比 Medium 和 High 好
-        // Medium 和 High 采样数多，过渡的区间大，在角色身上更容易出现 Perspective aliasing
-        shadowSamplingData.softShadowQuality = SOFT_SHADOW_QUALITY_LOW;
-        light.shadowAttenuation = SampleShadowmap(TEXTURE2D_ARGS(_MainLightShadowmapTexture, sampler_LinearClampCompare), shadowCoord, shadowSamplingData, shadowParams, false);
-        light.shadowAttenuation = lerp(light.shadowAttenuation, 1, GetMainLightShadowFade(positionWS));
-    #endif
+    // 我自己试下来，在角色身上 LowQuality 比 Medium 和 High 好
+    // Medium 和 High 采样数多，过渡的区间大，在角色身上更容易出现 Perspective aliasing
+    shadowSamplingData.softShadowQuality = SOFT_SHADOW_QUALITY_LOW;
+    light.shadowAttenuation = SampleShadowmap(TEXTURE2D_ARGS(_MainLightShadowmapTexture, sampler_LinearClampCompare), shadowCoord, shadowSamplingData, shadowParams, false);
+    light.shadowAttenuation = lerp(light.shadowAttenuation, 1, GetMainLightShadowFade(positionWS));
+#endif
 
-    #ifdef _LIGHT_LAYERS
-        if (!IsMatchingLightLayer(light.layerMask, GetMeshRenderingLayer()))
-        {
-            // 偷个懒，直接把强度改成 0
-            light.distanceAttenuation = 0;
-            light.shadowAttenuation = 0;
-        }
-    #endif
+#ifdef _LIGHT_LAYERS
+    if (!IsMatchingLightLayer(light.layerMask, GetMeshRenderingLayer()))
+    {
+        // 偷个懒，直接把强度改成 0
+        light.distanceAttenuation = 0;
+        light.shadowAttenuation = 0;
+    }
+#endif
 
     return light;
 }
 
-float4 GetMainLightBrightness(float3 inputMainLightColor, float brightnessFactor, float brightnessThresholdMin, float brightnessThresholdMax, float brightnessOffset)
+float3 GetMainLightBrightness(float3 inputMainLightColor, float brightnessFactor, float brightnessThresholdMin, float brightnessThresholdMax, float brightnessOffset)
 {
     float3 LightColor = inputMainLightColor.rgb * brightnessFactor;
-    #if _AUTO_Brightness_ON
-        LightColor = ColorBrightnessAdjustment(LightColor, brightnessOffset, brightnessThresholdMin, brightnessThresholdMax);
-    #endif
-    return float4(LightColor, 1);
+#if _AUTO_Brightness_ON
+    LightColor = ColorBrightnessAdjustment(LightColor, brightnessOffset, brightnessThresholdMin, brightnessThresholdMax);
+#endif
+    return LightColor;
 }
 
 float3 GetMainLightDiffuse(Light light, float brightnessFactor, float brightnessThresholdMin, float brightnessThresholdMax, float brightnessOffset, float mainLightColorUsage)
 {
     float3 color = light.color;
-    color = GetMainLightBrightness(color, brightnessFactor, brightnessThresholdMin, brightnessThresholdMax, brightnessOffset).rgb;
+    color = GetMainLightBrightness(color, brightnessFactor, brightnessThresholdMin, brightnessThresholdMax, brightnessOffset);
     color = lerp(desaturation(color.rgb), color.rgb, mainLightColorUsage);
     return color * light.distanceAttenuation;
 }
-
 
 // CharacterAdditionalLight --------------------------------------------------------------------------------------- //
 // ---------------------------------------------------------------------------------------------------------------- //
 void GetAdditionalLightDiffuse(float3 positionWS, float4 positionCS, float strength, inout float3 lightColor)
 {
 
-    #if defined(_ADDITIONAL_LIGHTS) || defined(_ADDITIONAL_LIGHTS_VERTEX)
-        uint lightsCount = GetAdditionalLightsCount();
-        #if defined(LIGHT_LOOP_BEGIN)
-            InputData inputData = (InputData)0;
-            inputData.normalizedScreenSpaceUV = GetNormalizedScreenSpaceUV(positionCS);
-            inputData.positionWS = positionWS;
-            LIGHT_LOOP_BEGIN(lightsCount)
-        #else
-            for(uint lightIndex = 0; lightIndex < lightsCount; lightIndex++)
-            {
-        #endif
+#if defined(_ADDITIONAL_LIGHTS) || defined(_ADDITIONAL_LIGHTS_VERTEX)
+    uint lightsCount = GetAdditionalLightsCount();
+#if defined(LIGHT_LOOP_BEGIN)
+    InputData inputData = (InputData)0;
+    inputData.normalizedScreenSpaceUV = GetNormalizedScreenSpaceUV(positionCS);
+    inputData.positionWS = positionWS;
+    LIGHT_LOOP_BEGIN(lightsCount)
+#else
+    for (uint lightIndex = 0; lightIndex < lightsCount; lightIndex++)
+    {
+#endif
 
-            Light light = GetAdditionalLight(lightIndex, positionWS);
-            #if defined(_LIGHT_LAYERS)
-                if (IsMatchingLightLayer(light.layerMask, GetMeshRenderingLayer()))
-            #endif
-            {
-                lightColor += light.color.rgb * light.distanceAttenuation * strength;
-            }
+    Light light = GetAdditionalLight(lightIndex, positionWS);
+#if defined(_LIGHT_LAYERS)
+    if (IsMatchingLightLayer(light.layerMask, GetMeshRenderingLayer()))
+#endif
+    {
+        lightColor += light.color.rgb * light.distanceAttenuation * strength;
+    }
 
-        #if defined(LIGHT_LOOP_END)
-            LIGHT_LOOP_END
-        #else
-            }
-        #endif
-    #endif
+#if defined(LIGHT_LOOP_END)
+    LIGHT_LOOP_END
+#else
+    }
+#endif
+#endif
 
-    #if defined(_ADDITIONAL_LIGHTS) && USE_FORWARD_PLUS
-        #if defined(URP_FP_DIRECTIONAL_LIGHTS_COUNT)
-            for(uint lightIndex = 0; lightIndex < min(URP_FP_DIRECTIONAL_LIGHTS_COUNT, MAX_VISIBLE_LIGHTS); lightIndex++)
-        #else
-            for(uint lightIndex = 0; lightIndex < min(_AdditionalLightsDirectionalCount, MAX_VISIBLE_LIGHTS); lightIndex++)
-        #endif
+#if defined(_ADDITIONAL_LIGHTS) && USE_FORWARD_PLUS
+#if defined(URP_FP_DIRECTIONAL_LIGHTS_COUNT)
+    for (uint lightIndex = 0; lightIndex < min(URP_FP_DIRECTIONAL_LIGHTS_COUNT, MAX_VISIBLE_LIGHTS); lightIndex++)
+#else
+    for (uint lightIndex = 0; lightIndex < min(_AdditionalLightsDirectionalCount, MAX_VISIBLE_LIGHTS); lightIndex++)
+#endif
+    {
+        Light light = GetAdditionalLight(lightIndex, positionWS);
+#if defined(_LIGHT_LAYERS)
+        if (IsMatchingLightLayer(light.layerMask, GetMeshRenderingLayer()))
+#endif
         {
-            Light light = GetAdditionalLight(lightIndex, positionWS);
-            #if defined(_LIGHT_LAYERS)
-                if (IsMatchingLightLayer(light.layerMask, GetMeshRenderingLayer()))
-            #endif
-            {
-                lightColor += light.color.rgb * light.distanceAttenuation * strength;
-            }
+            lightColor += light.color.rgb * light.distanceAttenuation * strength;
         }
-    #endif
+    }
+#endif
 }
-
 
 // GI ------------------------------------------------------------------------------------------------------------- //
 // ---------------------------------------------------------------------------------------------------------------- //
@@ -261,49 +255,48 @@ float3 CalculateGI(float3 baseColor, float diffuseThreshold, float3 sh, float in
     return intensity * lerp(1, baseColor, mainColorLerp) * lerp(desaturation(sh), sh, mainColorLerp) * diffuseThreshold;
 }
 
-
 // RampIndex ------------------------------------------------------------------------------------------------------ //
 // ---------------------------------------------------------------------------------------------------------------- //
 void OverrideRampLineIfNeed()
 {
-    #if !_CUSTOM_RAMP_MAPPING_ON
-        _RampV0 = 0;
-        _RampV1 = 1;
-        _RampV2 = 2;
-        _RampV3 = 3;
-        _RampV4 = 4;
-        _RampV5 = 5;
-        _RampV6 = 6;
-        _RampV7 = 7;
-    #endif
+#if !_CUSTOM_RAMP_MAPPING_ON
+    _RampV0 = 0;
+    _RampV1 = 1;
+    _RampV2 = 2;
+    _RampV3 = 3;
+    _RampV4 = 4;
+    _RampV5 = 5;
+    _RampV6 = 6;
+    _RampV7 = 7;
+#endif
 }
 
 half GetRampV(half matId)
 {
     OverrideRampLineIfNeed();
     return 0.0625 + 0.125 * lerp(lerp(lerp(lerp(lerp(lerp(lerp(
-        _RampV0,
-        _RampV1, step(0.125, matId)),
-        _RampV2, step(0.250, matId)),
-        _RampV3, step(0.375, matId)),
-        _RampV4, step(0.500, matId)),
-        _RampV5, step(0.625, matId)),
-        _RampV6, step(0.750, matId)),
-        _RampV7, step(0.875, matId));
+                                                              _RampV0,
+                                                              _RampV1, step(0.125, matId)),
+                                                          _RampV2, step(0.250, matId)),
+                                                     _RampV3, step(0.375, matId)),
+                                                _RampV4, step(0.500, matId)),
+                                           _RampV5, step(0.625, matId)),
+                                      _RampV6, step(0.750, matId)),
+                                 _RampV7, step(0.875, matId));
 }
 
 half GetRampLineIndex(half matId)
 {
     OverrideRampLineIfNeed();
     return lerp(lerp(lerp(lerp(lerp(lerp(lerp(
-        _RampV0,
-        _RampV1, step(0.125, matId)),
-        _RampV2, step(0.250, matId)),
-        _RampV3, step(0.375, matId)),
-        _RampV4, step(0.500, matId)),
-        _RampV5, step(0.625, matId)),
-        _RampV6, step(0.750, matId)),
-        _RampV7, step(0.875, matId));
+                                             _RampV0,
+                                             _RampV1, step(0.125, matId)),
+                                         _RampV2, step(0.250, matId)),
+                                    _RampV3, step(0.375, matId)),
+                               _RampV4, step(0.500, matId)),
+                          _RampV5, step(0.625, matId)),
+                     _RampV6, step(0.750, matId)),
+                _RampV7, step(0.875, matId));
 }
 
 half GetMetalIndex()
@@ -311,7 +304,6 @@ half GetMetalIndex()
     OverrideRampLineIfNeed();
     return _RampV4;
 }
-
 
 // NPR ------------------------------------------------------------------------------------------------------------ //
 // ---------------------------------------------------------------------------------------------------------------- //
